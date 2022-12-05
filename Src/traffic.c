c2abf75a6f9811f9bdfcc47a214530a72facbf0e
minhlang102 <minhlangstudy@gmail.com>
2022-12-05 22:25:45 +0700

    traffic, add fsm mode in main

--- Src/traffic.c
+++ Src/traffic.c
@@ -7,61 +7,15 @@
 
 #include "traffic.h"
 
-//void setColor1(int color) {
-//	switch (color) {
-//		case INIT:
-//			HAL_GPIO_WritePin(GPIOA, LED_RED1_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN1_Pin | LED_YELLOW1_Pin, GPIO_PIN_SET);
-//			break;
-//		case AUTO_RED:
-//			HAL_GPIO_WritePin(GPIOA, LED_RED1_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN1_Pin | LED_YELLOW1_Pin, GPIO_PIN_SET);
-//			break;
-//		case AUTO_GREEN:
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN1_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_RED1_Pin | LED_YELLOW1_Pin, GPIO_PIN_SET);
-//			break;
-//		case AUTO_YELLOW:
-//			HAL_GPIO_WritePin(GPIOA, LED_YELLOW1_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN1_Pin | LED_RED1_Pin, GPIO_PIN_SET);
-//			break;
-//		default:
-//			break;
-//	}
-//}
-//
-//void setColor2(int color) {
-//	switch (color) {
-//		case INIT:
-//			HAL_GPIO_WritePin(GPIOA, LED_RED2_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN2_Pin | LED_YELLOW2_Pin, GPIO_PIN_SET);
-//			break;
-//		case AUTO_RED:
-//			HAL_GPIO_WritePin(GPIOA, LED_RED2_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN2_Pin | LED_YELLOW2_Pin, GPIO_PIN_SET);
-//			break;
-//		case AUTO_GREEN:
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN2_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_RED2_Pin | LED_YELLOW2_Pin, GPIO_PIN_SET);
-//			break;
-//		case AUTO_YELLOW:
-//			HAL_GPIO_WritePin(GPIOA, LED_YELLOW2_Pin, GPIO_PIN_RESET);
-//			HAL_GPIO_WritePin(GPIOA, LED_GREEN2_Pin | LED_RED2_Pin, GPIO_PIN_SET);
-//			break;
-//		default:
-//			break;
-//	}
-//}
-
 void setColor1(int color) {
 	switch (color) {
 		case INIT:
-			HAL_GPIO_WritePin(LED1_1_GPIO_Port, LED1_1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED1_2_GPIO_Port, LED1_2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(LED1_1_GPIO_Port, LED1_1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED1_2_GPIO_Port, LED1_2_Pin, GPIO_PIN_RESET);
 			break;
 		case AUTO_RED:
 			HAL_GPIO_WritePin(LED1_1_GPIO_Port, LED1_1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED1_2_GPIO_Port, LED1_2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(LED1_2_GPIO_Port, LED1_2_Pin, GPIO_PIN_RESET);
 			break;
 		case AUTO_GREEN:
 			HAL_GPIO_WritePin(LED1_1_GPIO_Port, LED1_1_Pin, GPIO_PIN_RESET);
@@ -69,7 +23,7 @@ void setColor1(int color) {
 			break;
 		case AUTO_YELLOW:
 			HAL_GPIO_WritePin(LED1_1_GPIO_Port, LED1_1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED1_2_GPIO_Port, LED1_2_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED1_2_GPIO_Port, LED1_2_Pin, GPIO_PIN_SET);
 			break;
 		default:
 			break;
@@ -79,12 +33,12 @@ void setColor1(int color) {
 void setColor2(int color) {
 	switch (color) {
 		case INIT:
-			HAL_GPIO_WritePin(LED2_1_GPIO_Port, LED2_1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED2_2_GPIO_Port, LED2_2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(LED2_1_GPIO_Port, LED2_1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED2_2_GPIO_Port, LED2_2_Pin, GPIO_PIN_RESET);
 			break;
 		case AUTO_RED:
 			HAL_GPIO_WritePin(LED2_1_GPIO_Port, LED2_1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED2_2_GPIO_Port, LED2_2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(LED2_2_GPIO_Port, LED2_2_Pin, GPIO_PIN_RESET);
 			break;
 		case AUTO_GREEN:
 			HAL_GPIO_WritePin(LED2_1_GPIO_Port, LED2_1_Pin, GPIO_PIN_RESET);
@@ -92,7 +46,7 @@ void setColor2(int color) {
 			break;
 		case AUTO_YELLOW:
 			HAL_GPIO_WritePin(LED2_1_GPIO_Port, LED2_1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED2_2_GPIO_Port, LED2_2_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED2_2_GPIO_Port, LED2_2_Pin, GPIO_PIN_SET);
 			break;
 		default:
 			break;
