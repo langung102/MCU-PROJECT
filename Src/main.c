4a631444c0d6a149682fa1dd936675414d2ae421
minhlang102 <minhlangstudy@gmail.com>
2022-12-06 08:02:19 +0700

    change traffic

--- Src/main.c
+++ Src/main.c
@@ -74,7 +74,6 @@ static void MX_TIM3_Init(void);
   */
 int main(void)
 {
-	fsm_mode();
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -313,7 +312,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOA, LD2_Pin|LED_PDS2_Pin|LED1_1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LED_PDS1_Pin|LED1_2_Pin|LED2_2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LED_PDS1_Pin|LED1_2_Pin|LED2_2_Pin|LED2_1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -340,18 +339,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(BUTTON3_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LED_PDS1_Pin LED1_2_Pin LED2_2_Pin */
-  GPIO_InitStruct.Pin = LED_PDS1_Pin|LED1_2_Pin|LED2_2_Pin;
+  /*Configure GPIO pins : LED_PDS1_Pin LED1_2_Pin LED2_2_Pin LED2_1_Pin */
+  GPIO_InitStruct.Pin = LED_PDS1_Pin|LED1_2_Pin|LED2_2_Pin|LED2_1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LED2_1_Pin */
-  GPIO_InitStruct.Pin = LED2_1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(LED2_1_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
